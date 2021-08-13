30abd508cecdf632b12914740f354de2660eed5b
Camila Belmont <camilabelmont.alu@gmail.com>
2021-08-13 11:32:10 -0300

    Update

--- Ultimate Ninja Storm/src/Aparencia.java
+++ Ultimate Ninja Storm/src/Aparencia.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 
 public class Aparencia {
-    
     public void limparTela() throws InterruptedException, IOException{
         if (System.getProperty("os.name").contains("Windows")){ 
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); 
